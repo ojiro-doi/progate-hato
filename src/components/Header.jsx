769c3948cab4b19d36addb553579f36e1b71dcd3
Soon <137805644+Shun-Takamiya@users.noreply.github.com>
2024-05-04 00:47:26 +0900

    design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-
   return (
-    <header className="bg-green-400">
-      <div className="container flex mx-auto p-5 flex-col items-center md:flex-row">
-        <h1 className="font-medium">
-          <span className="text-xl">ランダムマップ</span>
+    <header>
+      <div className="container flex mx-auto flex-col md:flex-row">
+        <h1 className="container py-10 flex w-3/10 font-medium">
+          <span className="text-3xl ml-20">アプリ名</span>
         </h1>
-        <Link to={'/result'} className="md:ml-auto text-base border-b-2 border-gray-300">結果ページ</Link>
+        <div className="container py-10 bg-amber-500 flex w-7/10">
+          <Link to={"/result"} className="md:ml-auto text-3xl pl-20 mx-20">
+            HOME
+          </Link>
+          <Link to={"/result"} className="md:ml-auto text-3xl mx-20">
+            MAP
+          </Link>
+          <Link to={"/result"} className="md:ml-auto text-3xl mx-20">
+            RATE
+          </Link>
+        </div>
       </div>
     </header>
   );
 };
 
 export default Header;
-
-
--- src/components/InputFormBudget.jsx
+++ src/components/InputFormBudget.jsx
@@ -1,34 +1,66 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import piza from "./piza.png";
 
 const InputFormBudget = () => {
-  const [amount, setAmount] = useState('');
+  const [amount, setAmount] = useState("");
 
   const handleAmountChange = (event) => {
     setAmount(event.target.value);
   };
   return (
     <div className="container flex items-center flex-col">
-      <h1 className="text-3xl text-center mb-4">旅行の予算を入力してください</h1>
-      <form className="text-center mb-4">
-        <div className="flex justify-between items-center w-64 mx-auto">
-          <input
-            type="text"
-            value={amount}
-            onChange={handleAmountChange}
-            className="border-2 border-gray-300 rounded-md p-2 flex-grow text-center"
-            placeholder="予算を入力してください"
-          />
-          <span>円</span>
-        </div>
-      </form>
-      <div className="flex justify-center">
-        <div className="bg-blue-500 hover:bg-blue-700 text-white text-center my-1.5 inline-block rounded-lg">
-          <Link to={'/result'}>旅行先を探す</Link>
+      <img src={piza} className="absolute" />
+      <div className="relative">
+        <h1 className="text-5xl text-center mt-60 mb-4 margr">
+          行き先は
+          <span>
+            <input
+              type="text"
+              value={amount}
+              onChange={handleAmountChange}
+              className="border-2 border-gray-300 rounded-md p-2 flex-grow text-center"
+              placeholder="ランダム"
+            />
+          </span>
+          で決める！
+        </h1>
+        {/*<form className="text-center mb-4">
+          <div className="flex justify-between items-center w-64 mx-auto">
+            <input
+              type="text"
+              value={amount}
+              onChange={handleAmountChange}
+              className="border-2 border-gray-300 rounded-md p-2 flex-grow text-center"
+              placeholder="予算を入力してください"
+            />
+            <span>円</span>
+          </div>
+  </form>*/}
+        <h1 className="text-5xl text-center mt-20 mb-4 margr">
+          場所は
+          <span>
+            <input
+              type="text"
+              value={amount}
+              onChange={handleAmountChange}
+              className="border-2 border-gray-200 rounded-md p-2 flex-grow text-center"
+              placeholder="ランダム"
+            />
+          </span>
+          です！
+        </h1>
+        <div className="flex justify-center">
+          <div className="bg-red-500 hover:bg-green-700 text-5xl text-white text-center px-10 py-5 mx-20 my-10 inline-block rounded-lg">
+            <button>Start</button>
+          </div>
+          <div className="bg-red-500 hover:bg-green-700 text-5xl text-white text-center px-10 py-5 mx-20 my-10 inline-block rounded-lg">
+            <button>Stop</button>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default InputFormBudget
+export default InputFormBudget;
