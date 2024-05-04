1261cb59308f46f7af056a29bf51bb75c56bff42
gata <hayato.yamagata@icloud.com>
2024-05-04 23:48:02 +0900

    fix responsive

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -13,21 +13,15 @@ import { CountryContext } from '../Context/CountryProvider';
 
 
 function Result() {
-  const {selectedCountry,setSelectedCountry} = useContext(CountryContext);
-
-  console.log('result-selectedCountry.name:', selectedCountry.name); // ここでselectedCountryの値を確認
-
+  const { selectedCountry, setSelectedCountry } = useContext(CountryContext);
   const center = { lat: selectedCountry.lat, lng: selectedCountry.lng };
-
   const [videos, setVideos] = useState([]);
 
-  // selectedCountry が設定されるたびに自動で検索を行う
   useEffect(() => {
-    if (selectedCountry) {
-      // selectedCountryが空でない場合のみ検索を実行
-      onSearchYoutube(`${selectedCountry} 旅行`);
+    if (selectedCountry.name) {
+      onSearchYoutube(`${selectedCountry.name} 旅行`);
     }
-  }, [selectedCountry]); // selectedCountryが変更された時のみ検索を再実行
+  }, [selectedCountry]);
 
   const onSearchYoutube = (searchQuery) => {
     const url = `https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&q=${encodeURIComponent(
@@ -45,28 +39,16 @@ function Result() {
       });
   };
 
-  // useEffect(() => {
-  //   const randomCountry = countryList[Math.floor(Math.random() * countryList.length)];
-  //   setselectedCountry(randomCountry.name);
-  //   setCenter({ lat: randomCountry.lat, lng: randomCountry.lng });
-  // }, []); //初回レンダリング時のみ実行->空の配列を渡す
-
-  // console.log("redaer")
-
-
   return (
     <div>
       <Header />
       <div className="p-5">
-
-
-        <div className="flex mt-5">
-          <div className="w-3/5">
-            <h1 className=" text-5xl ml-4 mt-5 pb-3 border-b-2 border-black">
-
+        <div className="flex flex-col md:flex-row mt-5">
+          <div className="md:w-3/5">
+            <h1 className="text-5xl ml-4 mt-5 pb-3 border-b-2 border-black">
               Map
             </h1>
-              <Map selectedCountry={selectedCountry.name} center={center} />
+            <Map selectedCountry={selectedCountry.name} center={center} />
             <h1 className="text-5xl ml-4 mt-20 pb-3 border-b-2 border-black">
               About
             </h1>
