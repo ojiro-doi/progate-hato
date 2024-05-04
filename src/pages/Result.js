a3ef10b06ca14a0887faf8990b069a9c0450c519
Soon <137805644+Shun-Takamiya@users.noreply.github.com>
2024-05-04 17:30:59 +0900

    homepage

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -6,8 +6,6 @@ import axios from "axios";
 import { PresWiki } from "../components/PresWiki";
 import Map from "../components/Map";
 
-
-
 function Result() {
   const [countryName, setCountryName] = useState("");
   const [center, setCenter] = useState({ lat: 35.68, lng: 139.76 });
@@ -45,24 +43,16 @@ function Result() {
   }, []); //初回レンダリング時のみ実行->空の配列を渡す
 
   return (
-
-    
-
-
     <div className="compornents">
+      <div></div>
       <CountryListSelect
         countryName={countryName}
         setCountryName={setCountryName}
         setCenter={setCenter}
-        className="countrylistselect"
       />
-      <Map
-        countryName={countryName}
-        center={center}
-        className="map flex w-1/2"
-      />
-      <Youtube videos={videos} className="youtube flex w-1/2" />
-      <PresWiki countryName={countryName} className="prewiki" />
+      <Map countryName={countryName} center={center} />
+      <PresWiki countryName={countryName} />
+      <Youtube videos={videos} />
     </div>
   );
 }
