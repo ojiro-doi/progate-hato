0a9f2979517fba6a5ffde72e7bc5b3099e443d3d
aya0620a <aotomiyabi2525@gmail.com>
2024-05-04 17:43:45 +0900

    ホーム画面を修正

--- src/components/InputFormBudget.jsx
+++ src/components/InputFormBudget.jsx
@@ -4,6 +4,7 @@ import piza from "./piza.png";
 
 const InputFormBudget = () => {
   const [amount, setAmount] = useState("");
+  const [option, setOption] = useState('');
 
   const handleAmountChange = (event) => {
     setAmount(event.target.value);
@@ -12,51 +13,18 @@ const InputFormBudget = () => {
     <div className="container flex items-center flex-col">
       <img src={piza} className="absolute" />
       <div className="relative">
-        <h1 className="text-5xl text-center mt-60 mb-4 margr">
+        <h1 className="text-5xl text-center mt-40 mb-4 margr">
           行き先は
-          <span>
-            <input
-              type="text"
-              value={amount}
-              onChange={handleAmountChange}
-              className="border-2 border-gray-300 rounded-md p-2 flex-grow text-center"
-              placeholder="ランダム"
-            />
-          </span>
+          <select value={option} onChange={e => setOption(e.target.value)}>
+            <option value="ランダム" className="text-center">ランダム</option>
+            <option value="リスト" className="text-center">リスト</option>
+            <option value="質問">質問</option>
+          </select>
           で決める！
         </h1>
-        {/*<form className="text-center mb-4">
-          <div className="flex justify-between items-center w-64 mx-auto">
-            <input
-              type="text"
-              value={amount}
-              onChange={handleAmountChange}
-              className="border-2 border-gray-300 rounded-md p-2 flex-grow text-center"
-              placeholder="予算を入力してください"
-            />
-            <span>円</span>
-          </div>
-  </form>*/}
-        <h1 className="text-5xl text-center mt-20 mb-4 margr">
-          場所は
-          <span>
-            <input
-              type="text"
-              value={amount}
-              onChange={handleAmountChange}
-              className="border-2 border-gray-200 rounded-md p-2 flex-grow text-center"
-              placeholder="ランダム"
-            />
-          </span>
-          です！
-        </h1>
+        <div></div>
+        <h1 className="text-5xl text-center mt-20 mb-4 margr">君の旅先は！？</h1>
         <div className="flex justify-center">
-          <div className="bg-red-500 hover:bg-green-700 text-5xl text-white text-center px-10 py-5 mx-20 my-10 inline-block rounded-lg">
-            <button>Start</button>
-          </div>
-          <div className="bg-red-500 hover:bg-green-700 text-5xl text-white text-center px-10 py-5 mx-20 my-10 inline-block rounded-lg">
-            <button>Stop</button>
-          </div>
         </div>
       </div>
     </div>
