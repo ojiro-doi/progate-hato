a27814ab2fac547837d34740ab74bcebfc9dfbcd
Soon <137805644+Shun-Takamiya@users.noreply.github.com>
2024-05-05 14:19:03 +0900

    youtube modify

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,38 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import { ValuesContext } from "../Context/ValuesProvider";
 
 const Account = () => {
   // useContext からオブジェクトをそのまま受け取る
   const context = useContext(ValuesContext);
-  
+
   // 分割代入を使用して values と setValues を取り出す
   const { values, setValues } = context;
 
   const handleAmountChange = (event) => {
-    setValues(prevValues => ({
+    setValues((prevValues) => ({
       ...prevValues,
-      amount: event.target.value
+      amount: event.target.value,
     }));
   };
 
   const handleDaysChange = (event) => {
-    setValues(prevValues => ({
+    setValues((prevValues) => ({
       ...prevValues,
-      days: event.target.value
+      days: event.target.value,
     }));
   };
 
   const handlePeopleChange = (event) => {
-    setValues(prevValues => ({
+    setValues((prevValues) => ({
       ...prevValues,
-      people: event.target.value
+      people: event.target.value,
     }));
   };
 
   return (
     <div className="account_compornents">
       <div className="account_input w-1/2 mt-20 ml-20 my-5">
-        <h1 className="text-5xl border-b border-blue-400 font-serif pb-1 my-5">
+        <h1 className="text-5xl border-b border-blue-400 font-mono pb-1 my-5">
           予算
         </h1>
         <select
@@ -41,15 +41,15 @@ const Account = () => {
           onChange={handleAmountChange}
           className="border-2 border-gray-300 rounded-md p-2 mt-2 my-10 flex-grow text-center"
         >
-          <option>¥100,000</option>
-          <option>¥200,000</option>
-          <option>¥300,000</option>
-          <option>¥400,000</option>
-          <option>¥500,000</option>
-          <option>¥600,000</option>
-          <option>¥700,000以上</option>
+          <option>¥100,000（ドミノピザ 10枚分）</option>
+          <option>¥200,000（ドミノピザ 20枚分）</option>
+          <option>¥300,000（ドミノピザ 30枚分）</option>
+          <option>¥400,000（ドミノピザ 40枚分）</option>
+          <option>¥500,000（ドミノピザ 50枚分）</option>
+          <option>¥600,000（ドミノピザ 60枚分）</option>
+          <option>¥700,000以上（ドミノピザ 70枚分以上）</option>
         </select>
-        <h1 className="text-5xl border-b border-blue-400 font-serif pb-1 my-5">
+        <h1 className="text-5xl border-b border-blue-400 font-mono pb-1 my-5">
           宿泊日数
         </h1>
         <select
@@ -66,7 +66,7 @@ const Account = () => {
           <option>6泊</option>
           <option>7泊以上</option>
         </select>
-        <h1 className="text-5xl border-b border-blue-400 font-serif pb-1 my-5">
+        <h1 className="text-5xl border-b border-blue-400 font-mono pb-1 my-5">
           人数
         </h1>
         <select
